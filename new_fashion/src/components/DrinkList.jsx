30bcdc40acee3039318cfb91f70ad60c2b5b5a3f
Lane17027 <lanenichols17027@gmail.com>
2024-03-05 15:22:42 -0700

    able to render all drinks with their image and name

--- new_fashion/src/components/DrinkList.jsx
+++ new_fashion/src/components/DrinkList.jsx
@@ -1,7 +1,50 @@
+import axios from "axios"
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+
 export default function DrinkList () {
-  return(
-    <div>
-      <h3> This is the DrinkList </h3>
-    </div>
-  )
+  const navigate=useNavigate()
+  let showItem=(id)=>{
+    navigate(`${id}`)
+}
+
+  const [allDrinks, setAllDrinks]=useState([])
+
+  useEffect(()=>{
+    const getAllDrinks= async()=>{
+      const alcoholic= await axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic')
+      const nonalcoholic= await axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
+      const allTheDrinks=[...alcoholic.data.drinks, ...nonalcoholic.data.drinks]
+      console.log(alcoholic)
+
+      setAllDrinks(allTheDrinks)
+
+
+
+    }
+    getAllDrinks()
+  }, [])
+  console.log(allDrinks)
+
+  if(!allDrinks){
+    return <h1>Loading drinks, please wait</h1>
+  }
+
+  else {
+    return(
+      <div className="grid">
+        {allDrinks.map((drink, index)=>(
+          <div className="card" key={drink.idDrink} onClick={()=> showItem(drink.idDrink)}>
+            <h1>{drink.strDrink}</h1>
+            <img src={drink.strDrinkThumb} alt="" />
+          </div>
+        ))}
+
+      </div>
+    )
+
+  }
+
+
 }
