3c0191579f2b7511402957266daed994b7384dca
larsonscott89 <scottlarson0305@gmail.com>
2024-03-06 11:07:39 -0500

    header

--- new_fashion/src/components/Header.jsx
+++ new_fashion/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import Nav from './Nav'
-
+import '../componentsStyles/header.css'
 
 export default function Header () {
   return(
     <div className='header'>
-      <h1></h1>
       <Nav />
+      <form>
+        <label className='search' htmlFor='search'> Search </label>
+        <input type='text' id='search-bar' placeholder='Search for drink here!'/>
+      </form>
     </div>
   )
 }
